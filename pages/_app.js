e1b4208c41591fa538d1b7f5e8e6e64fd9cd7eb6
Iradukunda Isaac Auxile <69901906+Auxile-Isaac@users.noreply.github.com>
2024-06-07 17:01:08 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,28 +4,29 @@ import { ThemeProvider } from "@material-tailwind/react";
 import Head from 'next/head';
 import Script from 'next/script';
 
-
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider>
-      
       <ClerkProvider {...pageProps}>
         <Head>
-       <Script
-    strategy="beforeInteractive"
-    dangerouslySetInnerHTML={{
-      __html: `
-        (function() {
-          var g = document.createElement('script');
-          g.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBzN9QQjSZRS865tiM5bWwclv1kwngPvh0&libraries=places';
-          g.async = true;
-          g.defer = true;
-          document.head.appendChild(g);
-        })();
-      `,
-    }}
-  ></Script>
-      </Head>
+          <title>Your App Title</title>
+          {/* You can include other meta tags and links here */}
+        </Head>
+        <Script
+          id="google-maps-script"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                var g = document.createElement('script');
+                g.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBzN9QQjSZRS865tiM5bWwclv1kwngPvh0&libraries=places';
+                g.async = true;
+                g.defer = true;
+                document.head.appendChild(g);
+              })();
+            `,
+          }}
+        />
         <Component {...pageProps} />
       </ClerkProvider>
     </ThemeProvider>
